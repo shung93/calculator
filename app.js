9499b6a151ee08865fa4cd184023ddf54f61a056
Sam H <shung93@uw.edu>
2021-12-13 23:15:41 +0000

    Added functions for how to handle user inputs

--- app.js
+++ app.js
@@ -1,50 +1,117 @@
 function add(x, y) {
-    
     return x + y  
 };
 
-function subtract(x, y) {
-    
+function subtract(x, y) {  
     return x - y
 };
 
-function sum(numList) {
+function multiply(x, y) {
+    return x * y
+};
+
+function divide(x, y) {
+    return x / y
+};
+
+function clearInput() { 
+// if CE > then clear the current input
+// if C > then clear the final val
+};
+
+function operatorInput() { 
     
-    let num = 0
+    const buttonArray = document.getElementsByClassName("button operatorInput");
+    let operatorArray = [];
+
+    for (let i = 0; i < buttonArray.length; i++) {
+        operatorArray.push(buttonArray[i].innerHTML)
+
+        buttonArray[i].addEventListener("click", function(event) {
+            if (currNum.length > 0 && currInput.length < 3) {
+                currInput = [];
+                currInput.push(parseInt(currNum.join("")));
+                currNum = [];
+                currInput.push(buttonArray[i].innerHTML);
+            } else if (operatorArray.includes(currInput.at(-1))) {
+                //console.log('2')
+                currInput.pop();
+                currInput.push(buttonArray[i].innerHTML);
+            } else if (currInput.length === 0) {
+                //console.log('1')
+                return;
+            } else {
+                return;
+            }
+            // handle a situation where there you have something like [1, +] and a currNUM
+        });
+    };
+};
+
+function numInput() {
+
+    const buttonArray = document.getElementsByClassName("button numInput");
+
+    for (let i = 0; i < buttonArray.length; i++) {
+        buttonArray[i].addEventListener("click", function(event) {
+            if (currNum.length < 10) {
+                currNum.push(buttonArray[i].innerHTML)
+            }
+        });
+    };
+
+};
+
+function operatorSelector() {
     
-    for (let i = 0; i < numList.length; i++) {
-        num += numList[i]
+    let finalVal = 0;
+
+    if (currInput.includes('÷')) {
+        finalVal = divide(currInput[0], currInput[2])
+    } else if (currInput.includes('×')) {
+        finalVal = multiply(currInput[0], currInput[2])
+    } else if (currInput.includes('+')) {
+        finalVal = add(currInput[0], currInput[2])
+    } else if (currInput.includes('-')) {
+        finalVal = subtract(currInput[0], currInput[2])
     }
 
-    return num
+    return finalVal
+
 };
 
-function multiply(numList) {
-
-    let finalNum = numList.reduce((prevVal, nextVal) => prevVal * nextVal);
-
-    return finalNum
-};
-
-function power(x, y) {
+function evaluator() {
     
-    let numList = []
+    const buttonArray = document.getElementsByClassName("button evalInput");
 
-    for (let i = 0; i < y; i++) {
-        numList.push(x)
+    for (let i = 0; i < buttonArray.length; i++) {
+        buttonArray[i].addEventListener("click", function(event) {
+            
+            if (isNaN(parseInt(currNum.join("")))) {
+                return;
+            } else {
+                currInput.push(parseInt(currNum.join("")))
+                currInput = [operatorSelector()]
+                currNum = [operatorSelector()]
+            }
+
+        });
     };
 
-    const finalVal = numList.reduce((prevVal, nextVal) => prevVal * nextVal);
-    return finalVal
 };
 
-function factorial(num) {
-    let numList = []
-    
-    for (let i = num; i > 0; i--) {
-        numList.push(i)
-    };
+let currInput = []; 
+let currNum = [];
 
-    const finalVal = numList.reduce((prevVal, nextVal) => prevVal * nextVal);
-    return finalVal
+const buttonArray = document.getElementsByClassName("button test");
+
+for (let i = 0; i < buttonArray.length; i++) {
+    buttonArray[i].addEventListener("click", function(event) {
+        console.log(currInput)
+        console.log("currentNum:" + currNum)
+    });
 };
+
+operatorInput();
+numInput();
+evaluator();
